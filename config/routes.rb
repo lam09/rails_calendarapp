b134f6bd1c4289b94ae221e0949c986e04b8d30a
a.lam.tuan <lamtuananh278@gmail.com>
2019-10-31 16:45:16 +0100

    xx

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   #  post "all" => "events#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root "events"
   resource :events
 end
